79839590f9b0a572cd8361917919d6f0a8d46002
Swannhs <princeapurbask@gmail.com>
2021-03-16 23:31:04 +0600

    Configured app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Switch from "react-bootstrap/Switch";
+import {BrowserRouter, Route} from "react-router-dom";
+import LoginAction from "./components/Login/LoginAction";
+import LoginUI from "./components/Login/LoginUI";
+import Admin from "./layouts/Admin";
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    return (
+
+        <BrowserRouter>
+            {/*------------Public Route -------------------*/}
+            <Route path='/' component={LoginAction}/>
+            <Route exact path='/login' component={LoginUI}/>
+            {/*------------Public Route -------------------*/}
+
+            <Switch>
+                <Route path="/admin" render={(props) => <Admin {...props} />}/>
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App;
